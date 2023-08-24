1bf1d84355e7258042b88079eca6c617ec8d9684
Immanuel Soh <isoh2@huskers.unl.edu>
2023-08-24 11:32:54 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Immanuel Soh
+ *         isoh2@huskers.unl.edu
+ *
+ * Date: 2023/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Immanuel Soh\n");
+
+  return 0;
+}
